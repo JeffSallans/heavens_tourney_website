14c877101fb8de79e7c2c08d7966000fd938a965
Jeff Sallans <jeffsallans@ubuntu>
2014-03-02 01:38:18 -0500

    converting to demo version

--- basic_site/views.py
+++ basic_site/views.py
@@ -13,7 +13,7 @@ def index(request):
 
 	#get request
 	if request.method == 'GET':
-		template = loader.get_template('splash.html')
+		template = loader.get_template('home.html')
 
 	#post request
 	elif request.method == 'POST':
@@ -23,7 +23,40 @@ def index(request):
 			interest = Interest(email=email)
 			interest.save()
 		else:
-			template = loader.get_template('splash.html')
+			template = loader.get_template('home.html')
+
+	context = RequestContext(request, {
+		'error_message': error_message,
+	})
+
+	return HttpResponse(template.render(context))
+
+def demo(request):
+
+	error_message = "";
+	template = loader.get_template('demo.html')
+
+	context = RequestContext(request, {
+		'error_message': error_message,
+	})
+
+	return HttpResponse(template.render(context))
+
+def info(request):
+
+	error_message = "";
+	template = loader.get_template('test.html')
+
+	context = RequestContext(request, {
+		'error_message': error_message,
+	})
+
+	return HttpResponse(template.render(context))
+
+def contact(request):
+
+	error_message = "";
+	template = loader.get_template('contact.html')
 
 	context = RequestContext(request, {
 		'error_message': error_message,
--- heavens_tourney/urls.py
+++ heavens_tourney/urls.py
@@ -11,6 +11,10 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
 	url(r'^$', 'basic_site.views.index', name='home'),
+	url(r'^home$', 'basic_site.views.index', name='home'),
+	url(r'^demo$', 'basic_site.views.demo', name='demo'),
+	url(r'^monster_builds$', 'basic_site.views.info', name='info'),
+	url(r'^feedback$', 'basic_site.views.contact', name='contact'),
     url(r'^admin/', include(admin.site.urls)),
 )+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
